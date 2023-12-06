b81010a0a95cd0b970098504d42201f80c2fed61
sharifalmahmudjamil <smjamil111@gmail.com>
2023-12-05 20:31:16 -1200

    react world tour are add to my repositoty

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Countries from './components/Countries/countries'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1> React world tour</h1>
+      <Countries></Countries>
+      
+     
+    </>
+  )
+}
+
+export default App
